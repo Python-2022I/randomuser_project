e7b2532758bbddfb4e67d89bbc5d319363dacfe5
ZARIFMAC <uzbzarif@gmail.com>
2022-12-21 11:58:32 +0500

    first last full

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from random_user import RandomUser
 
 # Create a new instance of the RandomUser class
 user = RandomUser()
-print(user._data)
+# Print the first name of the user
+print(user.get_first_name())
+
+# Print the last name of the user
+print(user.get_last_name())
+
+# Print the full name of the user
+print(user.get_full_name())
--- random_user.py
+++ random_user.py
@@ -7,3 +7,22 @@ class RandomUser:
         """
         response = requests.get(self.baseURL).json()
         self._data = response["results"][0]
+
+
+    def get_first_name(self)->str:
+        """
+        Returns the first name of the user.
+        """
+        return self._data["name"]["first"]
+
+    def get_last_name(self)->str:
+        """
+        Returns the last name of the user.
+        """
+        return self._data["name"]["last"]
+
+    def get_full_name(self)->str:
+        """
+        Returns the full name of the user.
+        """
+        return f"{self.get_first_name()} {self.get_last_name()}"
