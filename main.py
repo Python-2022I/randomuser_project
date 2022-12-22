191fc88d3d84db006db2c33495514f1676d771da
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-12-22 23:27:43 +0500

    main.py is ready

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from random_user import RandomUser
+import pprint
 
 # Create a new instance of the RandomUser class
 user = RandomUser()
-# Print the first name of the user
-print(user.get_first_name())
+def static_method(num_users):
+    answer={
+        "first_name":user.get_first_name(),
+        "last_name":user.get_last_name(),
+        "full_name":user.get_full_name(),
+        "email":user.get_email(),
+        "phone":user.get_phone(),
+        "picture":user.get_picture()
+        
+    }
 
-# Print the last name of the user
-print(user.get_last_name())
 
-# Print the full name of the user
-print(user.get_full_name())
+    s=0
+    m=0
+    n=[]
+    while m!=num_users:
+        m+=1
+        n.append(answer)
+        s+=1
+        print(s)
+    return n
+
+pprint.pprint(static_method(5))
